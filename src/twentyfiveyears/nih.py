cc7ceea6c6e46fda0497fcde1db440a7531f8c10
Tom Pollard <tpollard@mit.edu>
2024-08-10 04:38:02 -0400

    Refactor for readability.

--- src/twentyfiveyears/nih.py
+++ src/twentyfiveyears/nih.py
@@ -4,7 +4,6 @@ nih.py
 This module contains functions used to prepare and process data
 extracted from NIH Exporter: https://reporter.nih.gov/exporter
 """
-
 import os
 import time
 
@@ -28,20 +27,17 @@ def combine_exporter_tables(folder, pattern, start_year):
         # so I am replacing  problematic characters
         df = pd.read_csv(
             os.path.join(folder, pattern + str(year) + ".csv"),
-            encoding_errors="replace",
+            dtype=str, encoding_errors="replace",
         )
         if df_combined is None:
             df_combined = df
         else:
-            # NOTE: getting a lot of DtypeWarning: Columns (#,#) have mixed
-            # types, consider specifying the dtype for
-            # each column within each folder
             df_combined = pd.concat([df_combined, df], ignore_index=True)
 
     return df_combined
 
 
-def standardize_nih_exporter_project_names(df):
+def standardize_pi_names(df):
     """
     Get the names from the NIH projects data into FIRST MIDDLE LAST format
     and remove the '(contact)' entries
@@ -70,7 +66,7 @@ def standardize_nih_exporter_project_names(df):
     return df
 
 
-def standardize_nih_exporter_publication_names(df):
+def standardize_author_names(df):
     """
     Get the names from the NIH publications data into FIRST MIDDLE LAST format
     """
@@ -94,47 +90,36 @@ def standardize_nih_exporter_publication_names(df):
     return df
 
 
-def get_nih_exporter_project_leader_names(projects_folder, start_year):
+def get_pi_names(projects):
     """
     Get a list of the PIs names who were granted funding from NIH.
     """
-    # Get the list of PIs from the NIH exporter
-    df_projects = combine_exporter_tables(
-        projects_folder, "RePORTER_PRJ_C_FY", start_year
-    )
+    projects = standardize_pi_names(projects)
+    projects = projects.drop_duplicates(subset="project_formatted_name")
 
-    df_projects = standardize_nih_exporter_project_names(df_projects)
-    df_projects = df_projects.drop_duplicates(subset="project_formatted_name")
-
-    # Prepare a list of formatted names from df_projects
+    # Prepare a list of formatted names from projects
     project_formatted_names = (
-        df_projects["project_formatted_name"].str.lower().dropna().tolist()
+        projects["project_formatted_name"].str.lower().dropna().tolist()
     )
-
-    print(f"# project_formatted_names = {len(project_formatted_names)}")
 
     return project_formatted_names
 
 
-def get_nih_exporter_publication_authors(publications_folder, start_year):
+def get_authors(publications):
     """
     Get a list of the authors who have published against NIH projects.
-    """
+
     # NOTE: have to manually rename the publication and links spreadsheets
     # after 2021 to remove the FY in front of YYYY
-    df_publications = combine_exporter_tables(
-        publications_folder, "RePORTER_PUB_C_", start_year
-    )
-
-    df_publications = standardize_nih_exporter_publication_names(df_publications)
-    df_publications = df_publications.drop_duplicates(
+    """
+    publications = standardize_author_names(publications)
+    publications = publications.drop_duplicates(
         subset="publication_formatted_name"
     )
 
     publication_formatted_names = (
-        df_publications["publication_formatted_name"].str.lower().dropna().tolist()
+        publications["publication_formatted_name"].str.lower().dropna().tolist()
     )
-    print(f"# publication_formatted_names = {len(publication_formatted_names)}")
 
     return publication_formatted_names
 
@@ -156,58 +141,41 @@ def best_jaro_winkler_match(user_name, nih_names):
     return best_score, best_match_name
 
 
-def check_nih_exporter_hits(
-    df_pn_users, projects_folder, publications_folder, start_year
-):
+def link_users(physionet_users, nih_users, match_group, limit=None):
     """
     Search for matches between the PhysioNet user names and the names in
     the projects and publications data from the NIH exporter.
     """
-    # Get the list of PhysioNet users
-    # df_pn_users = df_pn_users.iloc[1:11].copy() # For looping through
-    # fewer users during debug
-    user_names = df_pn_users["full_name"].str.lower().dropna().tolist()
-    print(f"# user names = {len(user_names)}")
-
-    # Get the list of project lead names from NIH exporter
-    project_formatted_names = get_nih_exporter_project_leader_names(
-        projects_folder, start_year
-    )
-
-    # Get the list of authors from publications associated with the NIH
-    # exporter projects
-    publication_formatted_names = get_nih_exporter_publication_authors(
-        publications_folder, start_year
-    )
-
-    # Time how long the fuzzy matching takes
     t0 = time.time()
+    user_names = physionet_users["physionet_name"].str.lower().dropna().tolist()
 
-    # Matching against the project DataFrame list
-    print(
-        "Searching for PhysioNet users who are PIs that were awarded grants in NIH Exporter..."
-    )
-    project_results = Parallel(n_jobs=-1)(
-        delayed(best_jaro_winkler_match)(user_name, project_formatted_names)
-        for user_name in tqdm(user_names)
-    )
-    project_best_scores, project_best_match_names = zip(*project_results)
-    df_pn_users.loc[:, "nih_project_best_score"] = project_best_scores
-    df_pn_users.loc[:, "nih_project_best_match_name"] = project_best_match_names
+    # Set limit when testing
+    if limit:
+        physionet_users = physionet_users[:limit]
+        nih_users = nih_users[:limit]
+        user_names = user_names[:limit]
 
-    # Matching against the publication DataFrame list
-    print(
-        "Searching for PhysioNet users who are published authors from NIH Exporter..."
-    )
-    publication_results = Parallel(n_jobs=-1)(
-        delayed(best_jaro_winkler_match)(user_name, publication_formatted_names)
-        for user_name in tqdm(user_names)
-    )
-    publication_best_scores, publication_best_match_names = zip(*publication_results)
-    df_pn_users.loc[:, "nih_publication_best_score"] = publication_best_scores
-    df_pn_users.loc[:, "nih_publication_best_match_name"] = publication_best_match_names
+    if match_group == "investigators":
+
+        results = Parallel(n_jobs=-1)(
+            delayed(best_jaro_winkler_match)(user_name, nih_users)
+            for user_name in tqdm(user_names)
+        )
+        best_scores, best_match_names = zip(*results)
+        physionet_users.loc[:, "matched_investigator_score"] = best_scores
+        physionet_users.loc[:, "matched_investigator_name"] = best_match_names
+
+    elif match_group == "authors":
+
+        results = Parallel(n_jobs=-1)(
+            delayed(best_jaro_winkler_match)(user_name, nih_users)
+            for user_name in tqdm(user_names)
+        )
+        best_scores, best_match_names = zip(*results)
+        physionet_users.loc[:, "matched_author_score"] = best_scores
+        physionet_users.loc[:, "matched_author_name"] = best_match_names
 
     t1 = time.time()
     print(f"Finished in {t1 - t0} seconds")
 
-    return df_pn_users
+    return physionet_users
